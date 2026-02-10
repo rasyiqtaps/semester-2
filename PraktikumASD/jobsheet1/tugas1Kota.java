ac4c0ac3ac394430aa8d4ef0dfad8a7306509e23
mashhisyam <beliakunindrayanggg@gmail.com>
2026-02-10 14:33:58 +0700

    susulan jobsheet 1

--- PraktikumASD/jobsheet1/tugas1Kota.java
+++ PraktikumASD/jobsheet1/tugas1Kota.java
@@ -1,6 +1,8 @@
 package PraktikumASD.jobsheet1;
 
-public class tugasKota {
+import java.util.Scanner;
+
+public class tugas1Kota {
     private static char[] kode = {'a', 'b', 'd', 'e', 'f', 'g', 'h', 'l', 'n', 't'};
     private static char[][] kota =      {{'B','A','N','T','E','N'},
                                         {'J','A','K','A','R','T','A'},
@@ -13,14 +15,28 @@ public class tugasKota {
                                         {'M','A','L','A','N','G'},
                                         {'T','E','G','A','L'}};
     public static void main(String[] args) {
-        System.out.println("berikut adalah kode dari beberapa kota: ");
-        for (int i = 0; i < kode.length; i++) {
-            for (int j = 0; j < kota.length; j++) {
-                for (int j2 = 0; j2 < kota[i].length; j2++) {
-                    System.out.print(kode[i] + "\n");
-                    System.out.print("");
+        pencarianKode();
+    }
+    static void pencarianKode(){
+        Scanner sc = new Scanner(System.in);
+        int counter = 0;
+        boolean p = true;
+        while (p) {
+            System.out.print("masukan kode kota: ");
+            char code = sc.next().charAt(0);
+            for (int i = 0; i < kode.length; i++) {
+                if (code == kode[i]) {
+                    counter = i;
+                    System.out.print("kodemu adalah kota ");
+                    for (int j = 0; j < kota[counter].length; j++) {
+                        System.out.print(kota[counter][j]);
+                    }
+                    p = false;
+                    
                 }
             }
         }
+
+        
     }
 }
---
+++ PraktikumASD/jobsheet1/tugas2ProgramMahasiswa.java
@@ -0,0 +1,148 @@
+package PraktikumASD.jobsheet1;
+import java.util.Scanner;
+
+public class tugas2ProgramMahasiswa {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukan Jadwal: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        String[][] arrayJadwalMahasiswa = new String[n][4];
+
+        int pos = 0;    
+        System.out.println("Selamat Datang Di Menu Pencatatan Jadwal Mahasiswa");
+        
+        while (true) {
+            System.out.println("pilih menu: \t 1: input data\t 2: tabel jadwal\t 3: cari data sesuai hari\t 4: cari data sesuai matakuliah");
+            System.out.print("===");
+            pos = sc.nextInt();
+            
+            switch (pos) {
+                case 1:
+                    inputJadwalKuliah(arrayJadwalMahasiswa);
+                    break;
+                    
+                    case 2:
+                        tabelJadwal(arrayJadwalMahasiswa);
+                        break;
+                        
+                        case 3:
+                            mencariHariKuliah(arrayJadwalMahasiswa);
+                            break;
+                            
+                            case 4:
+                                mencariJadwalKuliah(arrayJadwalMahasiswa);
+                                break;
+                                
+                                default:
+                                    System.out.println("kode invalid.");
+                                    continue;
+                                }
+                                }
+    }
+    
+    static void mencariJadwalKuliah(String[][] arrayJadwalMahasiswa) {
+    Scanner sc = new Scanner(System.in);
+    boolean ditemukan = false;
+
+    System.out.print("Masukkan nama mata kuliah yang dicari: ");
+    String key = sc.nextLine();
+
+    System.out.println("\nHasil pencarian mata kuliah: " + key);
+    System.out.println("===========================================================================");
+    System.out.printf("%-25s %-14s %-20s %-15s\n",
+            "Nama Mata Kuliah", "Ruang", "Hari Kuliah", "Jam Kuliah");
+    System.out.println("===========================================================================");
+
+    for (int i = 0; i < arrayJadwalMahasiswa.length; i++) {
+        if (arrayJadwalMahasiswa[i][0].equalsIgnoreCase(key)) {
+            System.out.printf("%-25s %-14s %-20s %-15s\n",
+                    arrayJadwalMahasiswa[i][0],
+                    arrayJadwalMahasiswa[i][1],
+                    arrayJadwalMahasiswa[i][2],
+                    arrayJadwalMahasiswa[i][3]);
+            ditemukan = true;
+        }
+    }
+
+    if (!ditemukan) {
+        System.out.println("Mata kuliah tidak ditemukan.");
+    }
+
+    System.out.println("===========================================================================");
+    }
+
+
+    static void mencariHariKuliah(String[][] arrayJadwalMahasiswa){
+            Scanner sc = new Scanner(System.in);
+            boolean ditemukan = false;
+
+            System.out.print("Hari: ");
+            String key = sc.nextLine();
+
+            System.out.println("\nHasil pencarian jadwal hari " + key + ":");
+            System.out.println("===========================================================================");
+            System.out.printf("%-25s %-14s %-20s %-15s\n",
+                    "Nama Mata Kuliah", "Ruang", "Hari Kuliah", "Jam Kuliah");
+            System.out.println("===========================================================================");
+
+            for (int i = 0; i < arrayJadwalMahasiswa.length; i++) {
+                if (arrayJadwalMahasiswa[i][2].equalsIgnoreCase(key)) {
+                    System.out.printf("%-25s %-14s %-20s %-15s\n",
+                            arrayJadwalMahasiswa[i][0],
+                            arrayJadwalMahasiswa[i][1],
+                            arrayJadwalMahasiswa[i][2],
+                            arrayJadwalMahasiswa[i][3]);
+                    ditemukan = true;
+                }
+            }
+
+            if (!ditemukan) {
+                System.out.println("Tidak ada jadwal kuliah pada hari tersebut.");
+            }
+
+        System.out.println("===========================================================================");
+    }
+
+    static void tabelJadwal(String[][] arrayJadwalMahasiswa) {
+        System.out.println();
+        System.out.printf("%-25s %-14s %-20s %-15s\n",
+                "Nama Mata Kuliah", "Ruang", "Hari Kuliah", "Jam Kuliah");
+
+        System.out.println("===========================================================================");
+
+        for (int i = 0; i < arrayJadwalMahasiswa.length; i++) {
+            System.out.printf("%-25s %-14s %-20s %-15s\n",
+                    arrayJadwalMahasiswa[i][0],
+                    arrayJadwalMahasiswa[i][1],
+                    arrayJadwalMahasiswa[i][2],
+                    arrayJadwalMahasiswa[i][3]);
+        }
+
+        System.out.println("===========================================================================");
+    }
+
+    static void inputJadwalKuliah(String[][] arrayJadwalMahasiswa) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < arrayJadwalMahasiswa.length; i++) {
+            System.out.println("Input Data jadwal ke-" + (i + 1));
+
+            System.out.print("Masukan Nama mata kuliah: ");
+            arrayJadwalMahasiswa[i][0] = sc.nextLine();
+
+            System.out.print("Ruang Kuliah: ");
+            arrayJadwalMahasiswa[i][1] = sc.nextLine();
+
+            System.out.print("Masukan Hari kuliah: ");
+            arrayJadwalMahasiswa[i][2] = sc.nextLine();
+
+            System.out.print("Jam kuliah: ");
+            arrayJadwalMahasiswa[i][3] = sc.nextLine();
+
+            System.out.println();
+        }
+    }
+}
