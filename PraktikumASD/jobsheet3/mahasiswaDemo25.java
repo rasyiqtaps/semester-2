f551813a45cdbd16755a0b4ff9998e8fff4b5e99
mashhisyam <beliakunindrayanggg@gmail.com>
2026-02-24 15:22:29 +0700

    Praktikum 3.3.2

---
+++ PraktikumASD/jobsheet3/mahasiswaDemo25.java
@@ -0,0 +1,31 @@
+package PraktikumASD.jobsheet3;
+import java.util.Scanner;
+
+public class mahasiswaDemo25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        mahasiswa25[] arrayOfMahasiswa = new mahasiswa25[3];
+        String dummy;
+
+        for (int i = 0; i < 3; i++) {
+            arrayOfMahasiswa[i] = new mahasiswa25();
+
+            System.out.println("Masukan data Mahasiswa ke-" + (i+1));
+            System.out.print("NIM\t: ");
+            arrayOfMahasiswa[i].nim = sc.nextLine();
+            System.out.print("Nama\t: ");
+            arrayOfMahasiswa[i].nama = sc.nextLine();
+            System.out.print("Kelas\t: ");
+            arrayOfMahasiswa[i].kelas = sc.nextLine();
+            System.out.print("IPK\t: ");
+            dummy = sc.nextLine();
+            arrayOfMahasiswa[i].ipk = Float.parseFloat(dummy);
+            System.out.println("-------------------------------------------");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Data Mahasiswa ke-" + (i + 1));
+            arrayOfMahasiswa[i].cetakInfo();
+        }
+    }
+}
